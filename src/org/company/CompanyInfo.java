84daff239413fdd22ff618c3b7957cfc7ff039b3
amarnath363 <mamarnath363@gmail.com>
2021-12-15 18:07:32 +0530

    message

---
+++ src/org/company/CompanyInfo.java
@@ -0,0 +1,28 @@
+package org.company;
+
+public class CompanyInfo {
+
+	private void CompanyName() {
+		System.out.println("infosys");
+	}
+	private void companyId() {
+	System.out.println("123");	
+	}
+	private void companyAddress() {
+	System.out.println("omr");
+
+	}
+	public static void main(String[] args) {
+		
+		CompanyInfo c=new CompanyInfo();
+		c.CompanyName();
+		c.companyId();
+		c.companyAddress();
+		
+		
+		
+	}
+	
+	
+	
+}
